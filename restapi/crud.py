dee2155a27c4db23cbfa7310c3511c0265903a7e
Conradgabe <gisuekebho5880@gmail.com>
2023-09-14 09:37:51 +0100

    perform the nbecessary crud operation

---
+++ restapi/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from . import models, schema
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_name(db: Session, user_name: str):
+    return db.query(models.User).filter(models.User.name == user_name).first()
+
+def create_user(db: Session, user: schema.UserBase):
+    db_user = models.User(name=user.name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
